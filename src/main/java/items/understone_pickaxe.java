9fb033eadc7dffcd5f01c1cf940a27b29093fa69
Ingushe <167761978+Sakalt@users.noreply.github.com>
2024-11-02 06:54:56 +0900

    Update understone_pickaxe.java

--- src/main/java/items/understone_pickaxe.java
+++ src/main/java/items/understone_pickaxe.java
@@ -4,9 +4,10 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.PickaxeItem;
 import net.minecraft.world.item.Item.Properties;
 import net.minecraft.world.item.Tiers;
+import net.minecraft.world.item.CreativeModeTabs;
 
 public class UnderstonePickaxe extends PickaxeItem {
     public UnderstonePickaxe() {
-        super(Tiers.DIAMOND, 4, -2.8F, new Properties().tab(/* Add your creative tab here */).durability(965)); // 耐久値を200に設定
+        super(Tiers.DIAMOND, 4, -2.8F, new Properties().tab(CreativeModeTabs.TOOLS).durability(965)); // タブ設定を追加
     }
 }
