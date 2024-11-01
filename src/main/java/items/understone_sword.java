4b0f2d54d53bc1ba0831e7c7d10f2e31c3793332
Ingushe <167761978+Sakalt@users.noreply.github.com>
2024-11-02 06:55:58 +0900

    Update understone_sword.java

--- src/main/java/items/understone_sword.java
+++ src/main/java/items/understone_sword.java
@@ -4,9 +4,10 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Item.Properties;
 import net.minecraft.world.item.Tiers;
+import net.minecraft.world.item.CreativeModeTabs;
 
 public class UnderstoneSword extends SwordItem {
     public UnderstoneSword() {
-        super(Tiers.DIAMOND, 7, -2.4F, new Properties().tab(/* Add your creative tab here */).durability(965)); // 耐久値を200に設定
+        super(Tiers.DIAMOND, 7, -2.4F, new Properties().tab(CreativeModeTabs.COMBAT).durability(965)); // タブ設定を追加
     }
 }
